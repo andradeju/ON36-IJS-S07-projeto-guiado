f381b1be0d77fa521fb7ef7f137b02d4bb69eb5d
andradeju <julliana.andrade08@gmail.com>
2024-08-17 16:11:14 -0300

    :feat: Add camada controller de alunos

---
+++ src/alunos/dto/create-aluno.dto.ts
@@ -0,0 +1,23 @@
+import { IsEmail, IsNotEmpty, IsNumber, IsString, Max, Min } from "class-validator";
+
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+
+export class CreateAlunoDto {
+  @IsString()
+  nome: string;
+
+  @IsString()
+  endereco: string;
+
+  @IsString()
+  telefone: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsNumber()
+  @Max(currentYear - 16)
+  anoNascimento: number;
+}
---
+++ src/alunos/entities/aluno.entity.ts
@@ -0,0 +1,14 @@
+export class Aluno {
+  nome: string;
+  endereco: string;
+  telefone: string;
+  email: string;
+  cursos: string[];
+
+  constructor(nome: string, endereco: string, telefone: string, email: string) {
+  this.nome = nome;
+  this.endereco = endereco;
+  this.telefone = telefone;
+  this.email = email;
+  }
+}
